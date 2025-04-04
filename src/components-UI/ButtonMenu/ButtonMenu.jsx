3f97b0f0980962d8bc64be4642307cca74f83ff3
Santigumi <santymillan95@gmail.com>
2025-04-04 12:08:37 -0500

    Styles updated

--- src/components-UI/ButtonMenu/ButtonMenu.jsx
+++ src/components-UI/ButtonMenu/ButtonMenu.jsx
@@ -1,16 +1,21 @@
 
 import Button from "@mui/material/Button"
 import DeleteIcon from "@mui/icons-material/Delete"
+import { Box } from "@mui/material"
 
 const ButtonMenu = () => {
     return (
         <Button sx={{
             display: "flex",
             flexDirection: "column",
-            fontSize: "large",
+            fontSize: "100"
         }} 
         variant="outlined"
-        startIcon = {<DeleteIcon fontSize="large" />}>Hola mi primer boton</Button>
+        startIcon = {
+        <Box sx={{ display: 'flex' }}>
+        <DeleteIcon fontSize="inherit" sx={{ fontSize: 50 }}/>
+        </Box>}
+        >Hola mi primer boton</Button>
     )
 }
 
