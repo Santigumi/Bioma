eb70aec801545cc6ebfcf514179e5fd3d431b7db
Santigumi <santymillan95@gmail.com>
2025-04-04 10:27:53 -0500

    Create ButtonMenu

---
+++ src/components-UI/ButtonMenu/ButtonMenu.jsx
@@ -0,0 +1,10 @@
+
+import Button from "@mui/material/Button"
+
+const ButtonMenu = () => {
+    return (
+        <Button variant="contained">Hola mi primer boton</Button>
+    )
+}
+
+export default ButtonMenu
