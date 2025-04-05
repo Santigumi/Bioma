77e98a375bc3651b2c848c82ae0ffe914119d5a9
Santigumi <santymillan95@gmail.com>
2025-04-05 17:46:15 -0500

    Update styles

---
+++ src/components-screens/Lesson/Lesson.jsx
@@ -0,0 +1,25 @@
+import { Box, Typography } from "@mui/material";
+const Lesson = ({ index, tituleText }) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "start",
+        alignItems: "center",
+        gap: 5,
+        height: "10%",
+        width: "100%",
+        borderRadius: 7.5,
+        paddingLeft: "3rem",
+        backgroundColor: "#FFEE8B",
+        boxShadow: 3,
+      }}
+    >
+      <Typography variant="h5">Lesson {index}</Typography>
+      <Typography variant="h6">{tituleText}</Typography>
+    </Box>
+  );
+};
+
+export default Lesson;
