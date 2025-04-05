633d7461ff9a8480e5257469dec37d31ec197023
Santigumi <santymillan95@gmail.com>
2025-04-05 10:12:49 -0500

    Page Journal titule

---
+++ src/screens/Journal.jsx
@@ -0,0 +1,7 @@
+import EstructureOne from "../components-screens/EstructureOne/EstructureOne";
+
+const Journal = () => {
+  return <EstructureOne titule={"Your Journal"}></EstructureOne>;
+};
+
+export default Journal;
