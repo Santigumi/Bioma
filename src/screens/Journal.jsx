ddb089f841c5aaaf92870a20df088c4ead9ee79a
Santigumi <santymillan95@gmail.com>
2025-04-05 12:23:27 -0500

    Update Journal

--- src/screens/Journal.jsx
+++ src/screens/Journal.jsx
@@ -1,9 +1,45 @@
-
+import { Box, Typography } from "@mui/material";
 
 const Journal = () => {
-  return <>
-  hola
-  </>;
+    return (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            width: "100%",
+            height: "100%",
+            boxSizing: "border-box",
+            gap: "5%",
+            paddingLeft: "2rem",
+            paddingRight: "2rem",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              height: "10%",
+              borderRadius: 7.5,
+              paddingLeft: "3rem",
+              backgroundColor: "#4AB8F0",
+              boxShadow: 3,
+            }}
+          >
+            <Typography variant="h3">Journal</Typography>
+          </Box>
+          <Box
+            sx={{
+              borderRadius: 10,
+              width: "100%",
+              minHeight: "70%",
+              backgroundColor: "#4AB8F0",
+              boxShadow: 3
+            }}
+          ></Box>
+        </Box>
+      );
 };
 
 export default Journal;
