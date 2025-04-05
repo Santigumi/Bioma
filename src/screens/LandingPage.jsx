7f5de15df68e5a9abf714c2edf6e66e038aebf3c
Mariana <marianitalizcanor@gmail.com>
2025-04-05 12:10:38 -0500

    feat navbar

--- src/screens/LandingPage.jsx
+++ src/screens/LandingPage.jsx
@@ -1,12 +1,15 @@
 import Navbar from '../components-screens/Navbar/Navbar';
 import Banner from '../components-screens/Banner/Banner';
+import backgroundImage from '../assets/backgrounds/fondolanding.jpg';
 import './LandingPage.css';
 
 const LandingPage = () => {
     return(
         <>
-            <Navbar></Navbar>
-            <Banner></Banner>
+            <div className='background' style={{ backgroundImage: `url(${backgroundImage})` }}>
+                <Navbar></Navbar>
+                <Banner></Banner>
+            </div>
 
         </>
     )
