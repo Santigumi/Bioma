747851d39cef25cf08420804043707fbe837f836
Santigumi <santymillan95@gmail.com>
2025-04-05 17:17:33 -0500

    Lessons create

---
+++ src/screens/Lessons.jsx
@@ -0,0 +1,46 @@
+
+import { Box, Typography, Tabs } from "@mui/material";
+
+const Lessons = () => {
+    return (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            width: "100%",
+            height: "100%",
+            boxSizing: "border-box",
+            gap: "5%",
+            paddingLeft: "2rem",
+            paddingRight: "2rem",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              height: "10%",
+              borderRadius: 7.5,
+              paddingLeft: "3rem",
+              backgroundColor: "#FFEE8B",
+              boxShadow: 3,
+            }}
+          >
+            <Typography variant="h4">Biomas</Typography>
+          </Box>
+          <Box
+            sx={{
+              borderRadius: 10,
+              width: "100%",
+              minHeight: "70%",
+              backgroundColor: "#FFEE8B",
+              boxShadow: 3
+            }}
+          ></Box>
+        </Box>
+      );
+};
+
+export default Lessons;
