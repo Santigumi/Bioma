5cc86696b547f3aee6067b55e9be480ab8b34f8f
Santigumi <santymillan95@gmail.com>
2025-04-05 18:41:40 -0500

    Added Grid

--- src/screens/Biomas.jsx
+++ src/screens/Biomas.jsx
@@ -1,55 +1,65 @@
-import { Box, Typography, Tabs } from "@mui/material";
+import { Box, Typography, Grid } from "@mui/material";
+import CardOne from "../components-screens/CardOne/CardOne";
 
 const Biomas = () => {
-    const arrayBiomas = [
-      {nombre:"Savannah"},
-      {nombre:"Moorland"},
-      {nombre:"Desert"},
-      {nombre:"Tropical Forest"},
-      {nombre:"Seagrass"},
-      {nombre:"Pelagic ecosystem"},
-      {nombre:"Mangroves"},
-      {nombre:"Reef"}
-    ]
-    return (
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            width: "100%",
-            height: "100%",
-            boxSizing: "border-box",
-            gap: "5%",
-            paddingLeft: "2rem",
-            paddingRight: "2rem",
-          }}
-        >
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              height: "10%",
-              borderRadius: 7.5,
-              paddingLeft: "3rem",
-              backgroundColor: "#FFEE8B",
-              boxShadow: 3,
-            }}
-          >
-            <Typography variant="h4">Biomas</Typography>
-          </Box>
-          <Box
-            sx={{
-              borderRadius: 10,
-              width: "100%",
-              minHeight: "70%",
-              backgroundColor: "#FFEE8B",
-              boxShadow: 3
-            }}
-          ></Box>
-        </Box>
-      );
+  const arrayBiomas = [
+    { nombre: "Savannah" },
+    { nombre: "Moorland" },
+    { nombre: "Desert" },
+    { nombre: "Tropical Forest" },
+    { nombre: "Seagrass" },
+    { nombre: "Pelagic ecosystem" },
+    { nombre: "Mangroves" },
+    { nombre: "Reef" },
+  ];
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        width: "100%",
+        height: "100%",
+        boxSizing: "border-box",
+        gap: "5%",
+        paddingLeft: "2rem",
+        paddingRight: "2rem",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          height: "10%",
+          borderRadius: 7.5,
+          paddingLeft: "3rem",
+          backgroundColor: "#FFEE8B",
+          boxShadow: 3,
+        }}
+      >
+        <Typography variant="h4">Biomas</Typography>
+      </Box>
+      <Grid
+        container
+        spacing={4}
+        sx={{
+          borderRadius: 10,
+          width: "100%",
+          minHeight: "70%",
+          boxShadow: 3,
+        }}
+      >
+        {arrayBiomas.map((bioma) => {
+          return (
+            <Grid item xl={4}>
+              <CardOne name={bioma.nombre}></CardOne>;
+            </Grid>
+          )
+        })}
+      </Grid>
+    </Box>
+  );
 };
 
 export default Biomas;
