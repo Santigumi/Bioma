2cb702f02fa537bf27b32efed2d50319c7edfea2
Santigumi <santymillan95@gmail.com>
2025-04-05 11:10:13 -0500

    Update styles

--- src/components-screens/EstructureOne/EstructureOne.jsx
+++ src/components-screens/EstructureOne/EstructureOne.jsx
@@ -1,6 +1,6 @@
 import { Box, Container, Typography } from "@mui/material";
 
-const EstructureOne = ({ titule }) => {
+const EstructureOne = ({ titule, bgcolor }) => {
   return (
     <Box
       sx={{
@@ -9,13 +9,10 @@ const EstructureOne = ({ titule }) => {
         justifyContent: "center",
         width: "100%",
         height: "100%",
-        backgroundColor: "blue",
         boxSizing: "border-box",
         gap: "5%",
-        paddingTop: "1rem",
         paddingLeft: "2rem",
         paddingRight: "2rem",
-        paddingBottom: "1rem"
       }}
     >
       <Box
@@ -24,13 +21,22 @@ const EstructureOne = ({ titule }) => {
           flexDirection: "column",
           justifyContent: "center",
           height: "10%",
-          backgroundColor: "purple",
+          borderRadius: 7.5,
+          paddingLeft: "3rem",
+          backgroundColor: bgcolor,
+          boxShadow: 3,
         }}
       >
         <Typography variant="h3">{titule}</Typography>
       </Box>
       <Box
-        sx={{ border: 1, borderRadius: 10, width: "100%", height: "70%" }}
+        sx={{
+          borderRadius: 10,
+          width: "100%",
+          height: "70%",
+          backgroundColor: bgcolor,
+          boxShadow: 3
+        }}
       ></Box>
     </Box>
   );
