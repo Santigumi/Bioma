636b99c7d2012f54c17f82761d385e11bf4df0b7
Santigumi <santymillan95@gmail.com>
2025-04-05 17:46:30 -0500

    Update lessons

--- src/screens/Lessons.jsx
+++ src/screens/Lessons.jsx
@@ -1,35 +1,49 @@
-
-import { Box, Typography, Tabs } from "@mui/material";
+import { Box } from "@mui/material";
 
 import Titule from "../components-screens/Titule/Titule";
+import Lesson from "../components-screens/Lesson/Lesson";
 
 const Lessons = () => {
-    return (
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            width: "100%",
-            height: "100%",
-            boxSizing: "border-box",
-            gap: "5%",
-            paddingLeft: "2rem",
-            paddingRight: "2rem",
-          }}
-        >
-         <Titule tituleText={"Savannah"}></Titule>
-          <Box
-            sx={{
-              borderRadius: 10,
-              width: "100%",
-              minHeight: "70%",
-              backgroundColor: "#FFEE8B",
-              boxShadow: 3
-            }}
-          ></Box>
-        </Box>
-      );
+  const arrayLesson = [
+    { index: 1, tituleText: "Introduction" },
+    { index: 2, tituleText: "Ecosystem" },
+    { index: 3, tituleText: "Comunitties" },
+    { index: 4, tituleText: "Problems" },
+  ];
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        width: "100%",
+        height: "100%",
+        boxSizing: "border-box",
+        gap: "5%",
+        paddingLeft: "2rem",
+        paddingRight: "2rem",
+      }}
+    >
+      <Titule tituleText={"Savannah"}></Titule>
+      <Box
+        sx={{
+          borderRadius: 10,
+          width: "100%",
+          minHeight: "70%",
+        }}
+      >
+        {arrayLesson.map((lesson) => {
+          return (
+            <Lesson
+              index={lesson.index}
+              tituleText={lesson.tituleText}
+            ></Lesson>
+          );
+        })}
+      </Box>
+    </Box>
+  );
 };
 
 export default Lessons;
