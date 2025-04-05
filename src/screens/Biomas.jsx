a129629f7ed63dcdeed3e2bb77a87c2f087809d0
Santigumi <santymillan95@gmail.com>
2025-04-05 10:12:36 -0500

    Page Biomas titule

---
+++ src/screens/Biomas.jsx
@@ -0,0 +1,7 @@
+import EstructureOne from "../components-screens/EstructureOne/EstructureOne";
+
+const Biomas = () => {
+  return <EstructureOne titule={"Biomas"}></EstructureOne>;
+};
+
+export default Biomas;
