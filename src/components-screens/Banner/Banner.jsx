24821f62492614f871a8b00826f0f6c829f883f6
Mariana <marianitalizcanor@gmail.com>
2025-04-05 12:43:12 -0500

    feat navbar

--- src/components-screens/Banner/Banner.jsx
+++ src/components-screens/Banner/Banner.jsx
@@ -1,23 +1,30 @@
+import './Banner.css';
 
 const Banner = () => {
     return(
-        <>
-            <div>
-                <img src="https://i.pinimg.com/736x/7f/0d/10/7f0d10e28387eb68f8bbdd67d3baf094.jpg" alt="Image Banner" />
-                <div>
-                    <h1>Bioma: Play and Learn</h1>
-                    <p> Bioma is an online learning platform focused on teaching about Colombian biodiversity. 
+        <div className="banner-container">
+            <div className="banner-image-container">
+                <img 
+                    src="https://i.pinimg.com/736x/7f/0d/10/7f0d10e28387eb68f8bbdd67d3baf094.jpg" 
+                    alt="Nature background" 
+                    className="banner-image"/>
+                
+                <div className="banner-content">
+                    <div>
+                        <h1>Bioma: Play and Learn</h1>
+                    </div>
+                    <p>
+                        Bioma is an online learning platform focused on teaching about Colombian biodiversity. 
                         This project is funded and implemented by the Ministry of Education in conjunction with the Ministry of Environment. 
-                        This project aims to teach adolescents relevant topics about biology, ecology, pollution, and more, with the goal of raising awareness 
-                        about the importance of our country's biodiversity.
+                        This project aims to teach adolescents relevant topics about biology, ecology, pollution, and more.
                     </p>
                 </div>
-                <div>
+                <div className="banner-buttons">
                     <button>Log in</button>
                     <button>Sign In</button>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
