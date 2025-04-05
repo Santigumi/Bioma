99131e32426c1e38c5d71e8f63df923e5d673757
Mariana <marianitalizcanor@gmail.com>
2025-04-05 15:51:54 -0500

    feat navbar

--- src/components-screens/Banner/Banner.jsx
+++ src/components-screens/Banner/Banner.jsx
@@ -8,16 +8,17 @@ const Banner = () => {
                     src="https://i.pinimg.com/736x/7f/0d/10/7f0d10e28387eb68f8bbdd67d3baf094.jpg" 
                     alt="Nature background" 
                     className="banner-image"/>
-                
                 <div className="banner-content">
                     <div>
                         <h1>Bioma: Play and Learn</h1>
                     </div>
-                    <p>
-                        Bioma is an online learning platform focused on teaching about Colombian biodiversity. 
-                        This project is funded and implemented by the Ministry of Education in conjunction with the Ministry of Environment. 
-                        This project aims to teach adolescents relevant topics about biology, ecology, pollution, and more.
-                    </p>
+                    <div className='content-p'>
+                        <p>
+                            Bioma is an online learning platform focused on teaching about Colombian biodiversity. 
+                            This project is funded and implemented by the Ministry of Education in conjunction with the Ministry of Environment. 
+                            This project aims to teach adolescents relevant topics about biology, ecology, pollution, and more.
+                        </p>
+                    </div>  
                 </div>
                 <div className="banner-buttons">
                     <button>Log in</button>
