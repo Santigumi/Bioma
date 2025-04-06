5c5f07cfe9ad2940dd9f84aaec2af12379b201a0
Santigumi <santymillan95@gmail.com>
2025-04-05 19:03:14 -0500

    Prettier

--- src/components-screens/CardOne/CardOne.jsx
+++ src/components-screens/CardOne/CardOne.jsx
@@ -1,9 +1,17 @@
 import { Box, Typography } from "@mui/material";
-const CardOne = ({name, image}) => {
+const CardOne = ({ name, image }) => {
   return (
-    <Box sx={{width}}>
-    <img src={image}></img>
-      <Typography>{name}</Typography>
+    <Box
+      sx={{
+        width: "325px",
+        height: "225px",
+        border: 1,
+        backgroundColor: "#00E773",
+        borderRadius: 5
+      }}
+    >
+      <img src={image}></img>
+      <Typography variant="h6">{name}</Typography>
     </Box>
   );
 };
