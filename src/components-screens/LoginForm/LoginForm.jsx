fe3767af382f43828e0b8a39c92125c580b3c471
Mariana <marianitalizcanor@gmail.com>
2025-04-06 20:01:43 -0500

    styles loginform

--- src/components-screens/LoginForm/LoginForm.jsx
+++ src/components-screens/LoginForm/LoginForm.jsx
@@ -1,37 +1,27 @@
-import { useForm } from 'react-hook-form';
-import pecesImg from '../../assets/backgrounds/peceslogin.jpg'
+import pecesImg from '../../assets/backgrounds/peceslogin.jpg';
 import './LoginForm.css';
 
 const LoginForm = () => {
-    // const {login, handleSubmit} = useForm();
-
-    //     const onSubmit = (data) => {
-    //         console.log(data);
-    //     };
-
     return (
         <div className="login-container">
-            <h2>Log In</h2>
-            <form className="register-form">
+            <form className="login-form">
+                <h2>Log in</h2>
                 <div className="form-group">
                     <label>Username</label>
-                    <input 
-                        type="text" 
-                        // {...login('username', { required: "Username is required" })}
-                        />
+                    <input type="text" />
                 </div>
                 <div className="form-group">
-                    <label>Password</label>
-                    <input 
-                        type="password" 
-                        // {...login('password', { 
-                        //     required: "Password is required"
-                        // })}
-                    />
+                    <label>Email</label>
+                    <input type="email" />
                 </div>
+                <div className='data-user'>
+                    <a href="#">Don't have an account? Sign in</a>
+                    <button className="submit-btn">Log in</button>
+                </div>
+                
             </form>
-            <div>
-                <img src= {pecesImg} alt="Coral picture" />
+            <div className="login-img">
+                <img src={pecesImg} alt="Coral reef background" />
             </div>
         </div>
     );
