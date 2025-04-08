22d554a58f21b5493ab0ca24ea034ef2f9f99df9
Santigumi <santymillan95@gmail.com>
2025-04-08 10:18:57 -0500

    Testing lessons

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 import LandingPage from "./screens/LandingPage";
 import Biomas from "./screens/Biomas";
 import Journal from "./screens/Journal";
+import Lessons from "./screens/Lessons";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Journal></Journal>
+      <Lessons></Lessons>
     </>
   );
 }
