a5988f7606ff8d74952762ea7f5d3a31339457ca
Santigumi <santymillan95@gmail.com>
2025-04-09 11:25:38 -0500

    Game testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import LandingPage from './screens/LandingPage';
 import RegisterPage from './screens/RegisterPage';
 import LoginPage from './screens/LoginPage';
-import Level from './components-Game/Level/Level'
+import Game from './screens/Game'
 import './App.css'
 
 function App() {
   return (
     <>
-      <Level></Level>
+      <Game></Game>
     </>
   );
 }
