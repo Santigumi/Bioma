9d884d1f727bec536fd13c3ad6edda48f5d0b329
Santigumi <santymillan95@gmail.com>
2025-04-09 10:23:11 -0500

    Update constans

--- src/components-Game/Level/Level.jsx
+++ src/components-Game/Level/Level.jsx
@@ -1,21 +1,13 @@
-
 import { Stage } from "react-konva";
 import Map from "../Map/Map";
 import Player from "../Player/Player";
-
-const grid = [
-  [1, 1, 1, 1, 1, 1],
-  [1, 0, 0, 0, 0, 1],
-  [1, 0, 1, 1, 0, 1],
-  [1, 0, 0, 0, 0, 1],
-  [1, 1, 1, 1, 1, 1],
-];
+import { GRID_MAP } from "../../utils/constants";
 
 const Level = () => {
   return (
-    <Stage width={grid[0].length * 50} height={grid.length * 50}>
-      <Map grid={grid} />
-      <Player grid={grid} />
+    <Stage width={GRID_MAP[0].length * 50} height={GRID_MAP.length * 50}>
+      <Map grid={GRID_MAP} />
+      <Player grid={GRID_MAP} />
     </Stage>
   );
 };
