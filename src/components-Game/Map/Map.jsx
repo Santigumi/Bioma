2930a30e9a079ee35dbce5bc09a2f870f56ba6d0
Santigumi <santymillan95@gmail.com>
2025-04-09 09:54:10 -0500

    Add Images to map

--- src/components-Game/Map/Map.jsx
+++ src/components-Game/Map/Map.jsx
@@ -4,39 +4,39 @@ import useImage from "use-image";
 const TITLE_SIZE = 50;
 
 const Map = ({ grid }) => {
-  const [pastoImage] = useImage("/assets/Sprites/Pasto.png");
+  const [pastoImage] = useImage("../src/assets/Sprites/Pasto.png");
   return (
     <Layer>
-      {grid.map((row, y)=>
-      row.map((cell, x) => {
-        const posX = x * TITLE_SIZE;
-        const posY = y * TITLE_SIZE;
+      {grid.map((row, y) =>
+        row.map((cell, x) => {
+          const posX = x * TITLE_SIZE;
+          const posY = y * TITLE_SIZE;
 
-        if(cell === 1){
-          return(
-            <KonvaImage>
+          if (cell === 1 && pastoImage) {
+            return (
+              <KonvaImage
+                key={`${x}-${y}`}
+                x={posX}
+                y={posY}
+                width={TITLE_SIZE}
+                height={TITLE_SIZE}
+                image={pastoImage}
+              ></KonvaImage>
+            );
+          }
+
+          return (
+            <Rect
               key={`${x}-${y}`}
               x={posX}
               y={posY}
               width={TITLE_SIZE}
               height={TITLE_SIZE}
-              image={pastoImage} 
-            </KonvaImage>
-          );
-        }
-
-        return (
-          <Rect>
-           key={`${x}-${y}`}
-              x={posX}
-              y={posY}
-              width={TITLE_SIZE}
-              height={TITLE_SIZE}
               fill="lightgreen"
-              stroke="gray" 
-          </Rect>
-        )
-      })
+              stroke="gray"
+            ></Rect>
+          );
+        })
       )}
     </Layer>
   );
