178e1b28e45d6cf44d345eb4e5c9e53d7eeecd41
Santigumi <santymillan95@gmail.com>
2025-04-09 11:08:18 -0500

    Title fixed

--- src/components-Game/Map/Map.jsx
+++ src/components-Game/Map/Map.jsx
@@ -1,9 +1,10 @@
 import { Layer, Image as KonvaImage, Rect } from "react-konva";
 import useImage from "use-image";
-import {TITLE_SIZE} from "../../utils/constants"
+import { TITLE_SIZE } from "../../utils/constants";
 
 const Map = ({ grid }) => {
   const [pastoImage] = useImage("../src/assets/Sprites/Pasto.png");
+  const [arbolImage] = useImage("../src/assets/Sprites/PlantaSavanah.png");
   return (
     <Layer>
       {grid.map((row, y) =>
@@ -11,30 +12,31 @@ const Map = ({ grid }) => {
           const posX = x * TITLE_SIZE;
           const posY = y * TITLE_SIZE;
 
-          if (cell === 1 && pastoImage) {
-            return (
-              <KonvaImage
-                key={`${x}-${y}`}
-                x={posX}
-                y={posY}
-                width={TITLE_SIZE}
-                height={TITLE_SIZE}
-                image={pastoImage}
-              ></KonvaImage>
-            );
-          }
-
-          return (
-            <Rect
-              key={`${x}-${y}`}
-              x={posX}
-              y={posY}
+          const title = (
+            <KonvaImage
+              key={`pasto-${x}-${y}`}
+              x={posX + TITLE_SIZE}
+              y={posY + TITLE_SIZE}
               width={TITLE_SIZE}
               height={TITLE_SIZE}
-              fill="lightgreen"
-              stroke="gray"
-            ></Rect>
+              image={pastoImage}
+            ></KonvaImage>
           );
+          const shouldRenderTree = cell === 1 && x % 2 === 0 && y % 2 === 0;
+
+          const tree =
+            shouldRenderTree && arbolImage ? (
+              <KonvaImage
+                key={`arbol-${x}-${y}`}
+                x={posX}
+                y={posY}
+                width={TITLE_SIZE * 2}
+                height={TITLE_SIZE * 2}
+                image={arbolImage}
+              />
+            ) : null;
+
+          return [title, tree];
         })
       )}
     </Layer>
