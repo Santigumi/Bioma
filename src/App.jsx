518f22ad13a3b9e686998d0af766e03e7815a540
Mariana <marianitalizcanor@gmail.com>
2025-04-09 16:16:09 -0500

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import LandingPage from './screens/LandingPage';
 import RegisterPage from './screens/RegisterPage';
 import LoginPage from './screens/LoginPage';
 import Game from './screens/Game'
-import ButtonMenu from './components-UI/ButtonMenu/ButtonMenu';
 import ProfilePage from './screens/ProfilePage';
 import './App.css'
 
