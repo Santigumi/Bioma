ecc8fec2be33c61df327d551438e4efaa8d7dc20
Mariana <marianitalizcanor@gmail.com>
2025-04-09 15:16:21 -0500

    style profile page

--- src/screens/ProfilePage.jsx
+++ src/screens/ProfilePage.jsx
@@ -4,16 +4,35 @@ import InfoCharacter from '../components-screens/InfoCharacter/InfoCharacter';
 import CharacterProfile from '../components-screens/CharacterProfile/CharacterProfile';
 import Capi from '../assets/characters/Capi.png';
 import backgroundImage from '../assets/backgrounds/fondoregister.jpg';
-import 
+import tree from '../assets/icons/tree-pine.png';
+import rail from '../assets/icons/rail-symbol.png' 
+import palm from '../assets/icons/tree-palm.png';
+import atom from '../assets/icons/atom.png';
+import sprout from '../assets/icons/sprout.png';
+import sailboat from '../assets/icons/sailboat.png';
 
 const ProfilePage = () => {
     return(
         <>
             <div className='background' style={{ backgroundImage: `url(${backgroundImage})` }}>
                 {/* <Navbar backgroundColor= "rgba(237, 124, 124, 1)"></Navbar> */}
-                <CharacterProfile nameCharacter = "Capybara" imgCharacter={Capi} ></CharacterProfile>
-                <InfoCharacter username = "Capysanti" charactersFound={Capi}></InfoCharacter>
-                <LevelProgress></LevelProgress>
+                <div>
+                    <CharacterProfile nameCharacter = "Capybara" imgCharacter={Capi} ></CharacterProfile>
+                </div>
+                <div>
+                    <InfoCharacter username = "Capysanti" charactersFound={Capi}></InfoCharacter>
+                </div>
+                <div>
+                    <LevelProgress iconProgress = {tree} percentage = "96%"></LevelProgress>
+                    <LevelProgress iconProgress = {tree} percentage = "45%" color= "rgba(255, 228, 67, 1)"></LevelProgress>
+                    <LevelProgress iconProgress = {sprout} percentage = "0%"></LevelProgress>
+                    <LevelProgress iconProgress = {palm} percentage = "16%" color= "rgba(255, 228, 67, 1)"></LevelProgress>
+                    <LevelProgress iconProgress = {tree} percentage = "5%" color= "rgba(0, 141, 213, 1)"></LevelProgress>
+                    <LevelProgress iconProgress = {sailboat} percentage = "15%" color= "rgba(71, 193, 255, 1)"></LevelProgress>                    <LevelProgress iconProgress = {tree} percentage = "5%" color= "rgba(0, 141, 213, 1)"></LevelProgress>
+                    <LevelProgress iconProgress = {rail} percentage = "0%" color= "rgba(71, 193, 255, 1)"></LevelProgress>
+                    <LevelProgress iconProgress = {atom} percentage = "0%" color= "rgba(0, 141, 213, 1)"></LevelProgress>
+                </div>
+
             </div>
         </>
     )
