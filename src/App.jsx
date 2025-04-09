1172b4d94602d5b73d05cb4f426b2f434cdd5f16
Santigumi <santymillan95@gmail.com>
2025-04-09 15:37:35 -0500

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
-import { useState } from 'react'
-import LandingPage from './screens/LandingPage';
-import RegisterPage from './screens/RegisterPage';
-import LoginPage from './screens/LoginPage';
-import Game from './screens/Game'
-import ProfilePage from './screens/ProfilePage';
-import './App.css'
-import Router from './Router/Index';
-
+import "./App.css";
+import Router from "./Router/Index";
 
 function App() {
   return (
