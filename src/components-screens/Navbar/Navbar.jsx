8f902dd2bf639a5ddc8901b5ae73641fc8a97256
Santigumi <santymillan95@gmail.com>
2025-04-09 15:35:27 -0500

    Update Navbar

--- src/components-screens/Navbar/Navbar.jsx
+++ src/components-screens/Navbar/Navbar.jsx
@@ -1,29 +1,32 @@
-import IconNav from '../IconNav/IconNav';
-import biomaIcon from '../../assets/icons/hoja.svg';
-import homeIcon from '../../assets/icons/home.svg';
-import journalIcon from '../../assets/icons/journal.svg';
-import profileIcon from '../../assets/icons/profile.svg';
-import "./Navbar.css"
-const Navbar = ({backgroundColor}) => {
-
-    return(
-        <div className='navbar'>
-            <ul className='navbar-list' style = {{backgroundColor: backgroundColor || `rgba(69, 196, 132, 1)`}}>
-                <li className='navbar-item'>
-                    <IconNav icon = {homeIcon} textIcon = "Home"></IconNav>
-                </li>
-                <li className='navbar-item'>
-                    <IconNav icon = {biomaIcon} textIcon = "Biomas"></IconNav>
-                </li>
-                <li className='navbar-item'>
-                    <IconNav icon = {journalIcon} textIcon = "Journal"></IconNav>
-                </li>
-                <li className='navbar-item'>
-                    <IconNav icon = {profileIcon} textIcon = "Profile"></IconNav>
-                </li>
-            </ul>
-        </div>
-    )
-}
+import IconNav from "../IconNav/IconNav";
+import biomaIcon from "../../assets/icons/hoja.svg";
+import homeIcon from "../../assets/icons/home.svg";
+import journalIcon from "../../assets/icons/journal.svg";
+import profileIcon from "../../assets/icons/profile.svg";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+const Navbar = ({ backgroundColor }) => {
+  return (
+    <nav className="navbar">
+      <ul
+        className="navbar-list"
+        style={{ backgroundColor: backgroundColor || `rgba(69, 196, 132, 1)` }}
+      >
+        <Link to="/" className="navbar-item">
+          <IconNav icon={homeIcon} textIcon="Home"></IconNav>
+        </Link>
+        <Link to="/Biomas" className="navbar-item">
+          <IconNav icon={biomaIcon} textIcon="Biomas"></IconNav>
+        </Link>
+        <Link to="/Journal" className="navbar-item">
+          <IconNav icon={journalIcon} textIcon="Journal"></IconNav>
+        </Link>
+        <Link to="/Register" className="navbar-item">
+          <IconNav icon={profileIcon} textIcon="Profile"></IconNav>
+        </Link>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
