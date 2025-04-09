640e989734f2cd41d9e2b83aa1ab3fbb2bcf78a4
Santigumi <santymillan95@gmail.com>
2025-04-09 15:04:15 -0500

    Testing Router

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import LandingPage from './screens/LandingPage';
 import RegisterPage from './screens/RegisterPage';
 import LoginPage from './screens/LoginPage';
 import Game from './screens/Game'
-import ButtonMenu from './components-UI/ButtonMenu/ButtonMenu';
 import ProfilePage from './screens/ProfilePage';
 import './App.css'
+import Router from './Router/Index';
 
 
 function App() {
   return (
     <>
-      <Game></Game>
+      <Router />
     </>
   );
 }
