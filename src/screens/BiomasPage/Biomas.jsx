bb9f533032fa79bb58e291f0c96398680daa6572
Santigumi <santymillan95@gmail.com>
2025-04-09 23:19:38 -0500

    Added routes to cards

--- src/screens/BiomasPage/Biomas.jsx
+++ src/screens/BiomasPage/Biomas.jsx
@@ -8,49 +8,49 @@ const Biomas = () => {
       nombre: "Savannah",
       image:
         "https://i.pinimg.com/736x/b6/59/72/b65972ef8b00e9abd0b825d03530442a.jpg",
-      route: "/Lessons"
+      route: "/Lessons",
     },
     {
       nombre: "Moorland",
       image:
         "https://i.pinimg.com/736x/d3/95/23/d395234cb987c50856d7106eb0ab20f5.jpg",
-      route: "/Lessons"
+      route: "/Lessons",
     },
     {
       nombre: "Desert",
       image:
         "https://i.pinimg.com/474x/f6/b2/8b/f6b28b752e832291a4eebab3acb9f486.jpg",
-        route: "/Lessons"
+      route: "/Lessons",
     },
     {
       nombre: "Tropical Forest",
       image:
         "https://i.pinimg.com/474x/dc/f9/50/dcf95015f213f050cca90622f8caf587.jpg",
-        route: "/Lessons"
+      route: "/Lessons",
     },
     {
       nombre: "Seagrass",
       image:
         "https://i.pinimg.com/474x/10/a8/96/10a896df46c691a055d509bb9b9d9c54.jpg",
-        route: "/Lessons"
+      route: "/Lessons",
     },
     {
       nombre: "Pelagic ecosystem",
       image:
         "https://animalesdecolombia.com.co/wp-content/uploads/2023/03/ecosistema-pelagico-1.jpg",
-        route: "/Lessons"
+      route: "/Lessons",
     },
     {
       nombre: "Mangroves",
       image:
         "https://animalesdecolombia.com.co/wp-content/uploads/2023/03/manglares.jpg",
-        route: "/Lessons"
+      route: "/Lessons",
     },
     {
       nombre: "Reef",
       image:
         "https://i.pinimg.com/474x/42/aa/11/42aa11b1c966179c8874cefe73c51497.jpg",
-        route: "/Lessons"
+      route: "/Lessons",
     },
   ];
   return (
@@ -65,7 +65,8 @@ const Biomas = () => {
         height: "100%",
         boxSizing: "border-box",
         paddingLeft: "10rem",
-        backgroundImage: "url('../src/assets/backgrounds/Biomas-Background.png')",
+        backgroundImage:
+          "url('../src/assets/backgrounds/Biomas-Background.png')",
       }}
     >
       <Box>
@@ -129,7 +130,11 @@ const Biomas = () => {
           {arrayBiomas.map((bioma) => {
             return (
               <Grid item xs={12}>
-                <CardOne name={bioma.nombre} image={bioma.image}></CardOne>
+                <CardOne
+                  name={bioma.nombre}
+                  image={bioma.image}
+                  direction={bioma.route}
+                ></CardOne>
               </Grid>
             );
           })}
