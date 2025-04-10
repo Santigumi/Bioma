3a8508e0cc56bf9f5938e043cc7cb6cff647ce68
Santigumi <santymillan95@gmail.com>
2025-04-09 23:45:33 -0500

    Update icons and styles

--- src/components-screens/Lesson/Lesson.jsx
+++ src/components-screens/Lesson/Lesson.jsx
@@ -1,25 +1,43 @@
 import { Box, Typography, Divider } from "@mui/material";
-const Lesson = ({ index, tituleText }) => {
+import LockIcon from "@mui/icons-material/Lock";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { useState } from "react";
+const Lesson = ({ index, tituleText, boolean }) => {
   return (
     <Box
       sx={{
         display: "flex",
         flexDirection: "row",
-        justifyContent: "start",
         alignItems: "center",
-        gap: 5,
+        gap: 105,
         height: "10%",
         width: "95%",
         borderRadius: 7.5,
         paddingLeft: "3rem",
         backgroundColor: "#FFEE8B",
         boxShadow: 3,
-        cursor: "pointer"
+        cursor: "pointer",
       }}
     >
-      <Typography variant="h6">Lesson {index}</Typography>
-      <Divider orientation="vertical" variant="middle" flexItem sx={{borderRightWidth: 2, color: "black", borderColor:"black"}}></Divider>
-      <Typography variant="h6">{tituleText}</Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          gap: 5,
+          height: "2rem",
+          width: "20rem",
+        }}
+      >
+        <Typography variant="h6">Lesson {index}</Typography>
+        <Divider
+          orientation="vertical"
+          variant="middle"
+          flexItem
+          sx={{ borderRightWidth: 2, color: "black", borderColor: "black" }}
+        ></Divider>
+        <Typography variant="h6">{tituleText}</Typography>
+      </Box>
+      <Box>{boolean === true ? <ArrowForwardIcon /> : <LockIcon />}</Box>
     </Box>
   );
 };
