3edf13ff28f3bcb0dfc5ea565551b5b543f439d8
Santigumi <santymillan95@gmail.com>
2025-04-09 22:38:07 -0500

    Images added

--- src/screens/BiomasPage/Biomas.jsx
+++ src/screens/BiomasPage/Biomas.jsx
@@ -3,14 +3,14 @@ import CardOne from "../../components-screens/CardOne/CardOne";
 import Navbar from "../../components-screens/Navbar/Navbar";
 const Biomas = () => {
   const arrayBiomas = [
-    { nombre: "Savannah" },
-    { nombre: "Moorland" },
-    { nombre: "Desert" },
-    { nombre: "Tropical Forest" },
-    { nombre: "Seagrass" },
-    { nombre: "Pelagic ecosystem" },
-    { nombre: "Mangroves" },
-    { nombre: "Reef" },
+    { nombre: "Savannah", image: "https://i.pinimg.com/736x/b6/59/72/b65972ef8b00e9abd0b825d03530442a.jpg"},
+    { nombre: "Moorland",  image: "https://i.pinimg.com/736x/d3/95/23/d395234cb987c50856d7106eb0ab20f5.jpg" },
+    { nombre: "Desert",  image: "https://i.pinimg.com/474x/f6/b2/8b/f6b28b752e832291a4eebab3acb9f486.jpg" },
+    { nombre: "Tropical Forest",  image: "https://i.pinimg.com/736x/f6/b2/8b/f6b28b752e832291a4eebab3acb9f486.jpg" },
+    { nombre: "Seagrass",  image: "https://i.pinimg.com/474x/10/a8/96/10a896df46c691a055d509bb9b9d9c54.jpg" },
+    { nombre: "Pelagic ecosystem",  image: "https://animalesdecolombia.com.co/wp-content/uploads/2023/03/ecosistema-pelagico-1.jpg" },
+    { nombre: "Mangroves",  image: "https://animalesdecolombia.com.co/wp-content/uploads/2023/03/manglares.jpg" },
+    { nombre: "Reef",  image: "https://i.pinimg.com/474x/42/aa/11/42aa11b1c966179c8874cefe73c51497.jpg" },
   ];
   return (
     <Box
@@ -67,7 +67,7 @@ const Biomas = () => {
           {arrayBiomas.map((bioma) => {
             return (
               <Grid item xs={12}>
-                <CardOne name={bioma.nombre}></CardOne>
+                <CardOne name={bioma.nombre} image={bioma.image}></CardOne>
               </Grid>
             );
           })}
