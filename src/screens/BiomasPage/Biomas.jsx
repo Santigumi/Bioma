0f91f4ec3ed0b04e4f19e79fb7f26293f9ceb70e
Santigumi <santymillan95@gmail.com>
2025-04-09 22:47:46 -0500

    Prettier

--- src/screens/BiomasPage/Biomas.jsx
+++ src/screens/BiomasPage/Biomas.jsx
@@ -3,14 +3,46 @@ import CardOne from "../../components-screens/CardOne/CardOne";
 import Navbar from "../../components-screens/Navbar/Navbar";
 const Biomas = () => {
   const arrayBiomas = [
-    { nombre: "Savannah", image: "https://i.pinimg.com/736x/b6/59/72/b65972ef8b00e9abd0b825d03530442a.jpg"},
-    { nombre: "Moorland",  image: "https://i.pinimg.com/736x/d3/95/23/d395234cb987c50856d7106eb0ab20f5.jpg" },
-    { nombre: "Desert",  image: "https://i.pinimg.com/474x/f6/b2/8b/f6b28b752e832291a4eebab3acb9f486.jpg" },
-    { nombre: "Tropical Forest",  image: "https://i.pinimg.com/736x/f6/b2/8b/f6b28b752e832291a4eebab3acb9f486.jpg" },
-    { nombre: "Seagrass",  image: "https://i.pinimg.com/474x/10/a8/96/10a896df46c691a055d509bb9b9d9c54.jpg" },
-    { nombre: "Pelagic ecosystem",  image: "https://animalesdecolombia.com.co/wp-content/uploads/2023/03/ecosistema-pelagico-1.jpg" },
-    { nombre: "Mangroves",  image: "https://animalesdecolombia.com.co/wp-content/uploads/2023/03/manglares.jpg" },
-    { nombre: "Reef",  image: "https://i.pinimg.com/474x/42/aa/11/42aa11b1c966179c8874cefe73c51497.jpg" },
+    {
+      nombre: "Savannah",
+      image:
+        "https://i.pinimg.com/736x/b6/59/72/b65972ef8b00e9abd0b825d03530442a.jpg",
+    },
+    {
+      nombre: "Moorland",
+      image:
+        "https://i.pinimg.com/736x/d3/95/23/d395234cb987c50856d7106eb0ab20f5.jpg",
+    },
+    {
+      nombre: "Desert",
+      image:
+        "https://i.pinimg.com/474x/f6/b2/8b/f6b28b752e832291a4eebab3acb9f486.jpg",
+    },
+    {
+      nombre: "Tropical Forest",
+      image:
+        "https://i.pinimg.com/474x/dc/f9/50/dcf95015f213f050cca90622f8caf587.jpg",
+    },
+    {
+      nombre: "Seagrass",
+      image:
+        "https://i.pinimg.com/474x/10/a8/96/10a896df46c691a055d509bb9b9d9c54.jpg",
+    },
+    {
+      nombre: "Pelagic ecosystem",
+      image:
+        "https://animalesdecolombia.com.co/wp-content/uploads/2023/03/ecosistema-pelagico-1.jpg",
+    },
+    {
+      nombre: "Mangroves",
+      image:
+        "https://animalesdecolombia.com.co/wp-content/uploads/2023/03/manglares.jpg",
+    },
+    {
+      nombre: "Reef",
+      image:
+        "https://i.pinimg.com/474x/42/aa/11/42aa11b1c966179c8874cefe73c51497.jpg",
+    },
   ];
   return (
     <Box
@@ -30,15 +62,15 @@ const Biomas = () => {
         <Navbar />
       </Box>
 
-      <Box ClassName="Estructure"
-      sx={{
-        display: "flex",
-        flexDirection: "Column",
-        height: "100%",
-        width: "100vw",
-        border: 3,
-        borderColor: "red"
-      }}
+      <Box
+        ClassName="Estructure"
+        sx={{
+          display: "flex",
+          flexDirection: "Column",
+          justifyContent: "space-around",
+          height: "99vh",
+          width: "100vw"
+        }}
       >
         <Box
           sx={{
@@ -59,9 +91,10 @@ const Biomas = () => {
           columnSpacing={6}
           spacing={4}
           sx={{
-            borderRadius: 10,
+            padding: "2rem",
             width: "100%",
-            minHeight: "70%",
+            minHeight: "80vh",
+            boxSizing: "border-box",
           }}
         >
           {arrayBiomas.map((bioma) => {
