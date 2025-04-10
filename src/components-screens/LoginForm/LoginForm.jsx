764d8fd31a582ccf2d01d52400dc47e64fe06c63
Santigumi <santymillan95@gmail.com>
2025-04-10 12:58:52 -0500

    Update css

--- src/components-screens/LoginForm/LoginForm.jsx
+++ src/components-screens/LoginForm/LoginForm.jsx
@@ -1,30 +1,32 @@
-import pecesImg from '../../assets/backgrounds/peceslogin.jpg';
-import './LoginForm.css';
+import pecesImg from "../../assets/backgrounds/peceslogin.jpg";
+import "./LoginForm.css";
+import { Link } from "react-router-dom";
 
 const LoginForm = () => {
-    return (
-        <div className="login-container">
-            <form className="login-form">
-                <h2>Log in</h2>
-                <div className="form-group">
-                    <label>Username</label>
-                    <input type="text" />
-                </div>
-                <div className="form-group">
-                    <label>Email</label>
-                    <input type="email" />
-                </div>
-                <div className='data-user'>
-                    <a href="#">Don't have an account? Sign in</a>
-                    <button className="submit-btn">Log in</button>
-                </div>
-                
-            </form>
-            <div className="login-img">
-                <img src={pecesImg} alt="Coral reef background" />
-            </div>
+  return (
+    <div className="login-container">
+      <form className="login-form">
+        <h2>Log in</h2>
+        <div className="form-group">
+          <label>Username</label>
+          <input type="text" />
         </div>
-    );
+        <div className="form-group">
+          <label>Email</label>
+          <input type="email" />
+        </div>
+        <div className="data-user">
+          <Link to="/Register">Don't have an account? Create one</Link>
+          <Link id="Signin" to="/Profile">
+            Log in
+          </Link>
+        </div>
+      </form>
+      <div className="login-img">
+        <img src={pecesImg} alt="Coral reef background" />
+      </div>
+    </div>
+  );
 };
 
 export default LoginForm;
