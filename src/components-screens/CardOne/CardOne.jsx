6e2c7be23bdf26fb2cf998331869032cd0fd78ae
Santigumi <santymillan95@gmail.com>
2025-04-09 23:04:38 -0500

    Added Router

--- src/components-screens/CardOne/CardOne.jsx
+++ src/components-screens/CardOne/CardOne.jsx
@@ -1,37 +1,40 @@
 import { Box, Typography } from "@mui/material";
-const CardOne = ({ name, image }) => {
+import { Link } from "react-router-dom";
+const CardOne = ({ name, image, direction }) => {
   return (
-    <Box
-      sx={{
-        width: "285px",
-        height: "205px",
-        backgroundColor: "#00E773",
-        borderRadius: 5,
-        boxShadow: 8,
-        cursor: "pointer"
-      }}
-    >
-      <img
-        style={{
-          width: "285px",
-          height: "160px",
-          borderRadius: 5
-        }}
-        src={image}
-      ></img>
-      <Typography
-        variant="h6"
+    <Link to={direction}>
+      <Box
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "35px",
+          width: "285px",
+          height: "205px",
+          backgroundColor: "#00E773",
+          borderRadius: 5,
+          boxShadow: 8,
+          cursor: "pointer",
         }}
       >
-        {name}
-      </Typography>
-    </Box>
+        <img
+          style={{
+            width: "285px",
+            height: "160px",
+            borderRadius: 5,
+          }}
+          src={image}
+        ></img>
+        <Typography
+          variant="h6"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "35px",
+          }}
+        >
+          {name}
+        </Typography>
+      </Box>
+    </Link>
   );
 };
 export default CardOne;
