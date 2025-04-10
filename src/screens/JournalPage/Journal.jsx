6480f9238d8a87d8ecc21fe7e63f3b40b7c01081
Santigumi <santymillan95@gmail.com>
2025-04-10 10:41:27 -0500

    Fix styles

--- src/screens/JournalPage/Journal.jsx
+++ src/screens/JournalPage/Journal.jsx
@@ -42,99 +42,112 @@ const Journal = () => {
     <Box
       sx={{
         display: "flex",
-        flexDirection: "column",
-        justifyContent: "start",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
         width: "100%",
         height: "100%",
         boxSizing: "border-box",
-        gap: "2.5%",
-        paddingTop: "1rem",
-        paddingLeft: "2rem",
-        paddingRight: "2rem",
+        paddingLeft: "10rem",
+        backgroundImage:
+        "url('../src/assets/backgrounds/Journal-Background.png')",
       }}
     >
       <Box>
         <Navbar />
       </Box>
+
       <Box
+        ClassName="Estructure"
         sx={{
           display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          height: "10%",
-          borderRadius: 7.5,
-          paddingLeft: "3rem",
-          backgroundColor: "#4AB8F0",
-          boxShadow: 3,
+          flexDirection: "Column",
+          justifyContent: "space-around",
+          height: "90vh",
+          width: "85vw",
+          border: 1
         }}
       >
-        <Typography variant="h4">Journal</Typography>
-      </Box>
-      <Box
-        sx={{
-          width: "100%",
-          minHeight: "75%",
-          boxSizing: "border-box",
-        }}
-      >
-        <Box>
-          <Tabs onChange={handleChange}>
-            <Tab
-              sx={{
-                backgroundColor: "#FFE549",
-                borderTopLeftRadius: 10,
-                borderTopRightRadius: 25,
-                color: "black",
-                "&.Mui-selected": {
-                  color: "F7F8F9",
-                },
-              }}
-              label="Ecosystem"
-              {...a11yProps(0)}
-            ></Tab>
-            <Tab
-              sx={{
-                backgroundColor: "#D62828",
-                borderTopLeftRadius: 10,
-                borderTopRightRadius: 25,
-              }}
-              label="Animals"
-              {...a11yProps(1)}
-            ></Tab>
-            <Tab
-              sx={{
-                backgroundColor: "#00E773",
-                borderTopLeftRadius: 10,
-                borderTopRightRadius: 25,
-              }}
-              label="Plants"
-              {...a11yProps(2)}
-            ></Tab>
-          </Tabs>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            height: "10%",
+            borderRadius: 7.5,
+            paddingLeft: "3rem",
+            backgroundColor: "#4AB8F0",
+            boxShadow: 3,
+          }}
+        >
+          <Typography variant="h4">Journal</Typography>
         </Box>
         <Box
           sx={{
-            padding: "2rem",
-            borderRadius: 10,
-            borderTopLeftRadius: 0,
             width: "100%",
-            minHeight: "100%",
-            backgroundColor: "#F7F8F9",
-            border: 4,
-            borderColor: "#4AB8F0",
-            boxShadow: 3,
+            minHeight: "75%",
             boxSizing: "border-box",
           }}
         >
-          <CustomTabPanel value={value} index={0}>
-            Ecosystem are empty
-          </CustomTabPanel>
-          <CustomTabPanel value={value} index={1}>
-            Animals are empty
-          </CustomTabPanel>
-          <CustomTabPanel value={value} index={2}>
-            Plants are empty
-          </CustomTabPanel>
+          <Box>
+            <Tabs onChange={handleChange}>
+              <Tab
+                sx={{
+                  backgroundColor: "#FFE549",
+                  borderTopLeftRadius: 10,
+                  borderTopRightRadius: 25,
+                  color: "black",
+                  "&.Mui-selected": {
+                    color: "F7F8F9",
+                  },
+                }}
+                label="Ecosystem"
+                {...a11yProps(0)}
+              ></Tab>
+              <Tab
+                sx={{
+                  backgroundColor: "#D62828",
+                  borderTopLeftRadius: 10,
+                  borderTopRightRadius: 25,
+                }}
+                label="Animals"
+                {...a11yProps(1)}
+              ></Tab>
+              <Tab
+                sx={{
+                  backgroundColor: "#00E773",
+                  borderTopLeftRadius: 10,
+                  borderTopRightRadius: 25,
+                }}
+                label="Plants"
+                {...a11yProps(2)}
+              ></Tab>
+            </Tabs>
+          </Box>
+          <Box
+            sx={{
+              padding: "2rem",
+              borderRadius: 10,
+              borderTopLeftRadius: 0,
+              width: "100%",
+              minHeight: "100%",
+              backgroundColor: "#F7F8F9",
+              border: 4,
+              borderColor: "#4AB8F0",
+              boxShadow: 3,
+              boxSizing: "border-box",
+            }}
+          >
+            <CustomTabPanel value={value} index={0}>
+              Ecosystem are empty
+            </CustomTabPanel>
+            <CustomTabPanel value={value} index={1}>
+              Animals are empty
+            </CustomTabPanel>
+            <CustomTabPanel value={value} index={2}>
+              Plants are empty
+            </CustomTabPanel>
+          </Box>
         </Box>
       </Box>
     </Box>
