ae3425ef894559091f2a9d5b580a27d8bf8e8f96
Santigumi <santymillan95@gmail.com>
2025-05-01 17:38:20 -0500

    Reubicated GamePage

---
+++ src/screens/GamePage/Game.jsx
@@ -0,0 +1,81 @@
+import { Box, Typography } from "@mui/material";
+import Level from "../../components-Game/Level/Level";
+import Navbar from "../../components-screens/Navbar/Navbar";
+const Game = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        width: "100%",
+        height: "100%",
+        boxSizing: "border-box",
+        paddingLeft: "10rem",
+        backgroundImage:
+          "url('../src/assets/backgrounds/Biomas-Background.png')",
+      }}
+    >
+      <Box>
+        <Navbar backgroundColor="#FFEE8B"/>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "Column",
+          justifyContent: "space-between",
+          height: "95vh",
+          width: "85vw",
+        }}
+      >
+        <Box
+          className="Estructure"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            height: "10%",
+            width: "90%",
+            borderRadius: 7.5,
+            paddingLeft: "3rem",
+            backgroundColor: "#FFEE8B",
+            boxShadow: 3,
+          }}
+        >
+          <Typography variant="h4">Savannah - lesson 1</Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            height: "6%",
+            width: "90%",
+            borderRadius: 7.5,
+            paddingLeft: "3rem",
+            backgroundColor: "#FFEE8B",
+            boxShadow: 3,
+          }}
+        >
+          <Typography variant="body1">
+            Learn the species that live in this place: Control de Capybara and
+            see how is their life.
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            width: "100%",
+            height: "75%",
+          }}
+        >
+          <Level></Level>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Game;
