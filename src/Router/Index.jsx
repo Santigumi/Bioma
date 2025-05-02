aea7c2720cc3092499ffc60b48a431966b0b815d
Santigumi <santymillan95@gmail.com>
2025-05-02 09:31:11 -0500

    Fix routes

--- src/Router/Index.jsx
+++ src/Router/Index.jsx
@@ -1,17 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "../screens/LandingPage";
+import LandingPage from "../screens/LandingPage/LandingPage";
+import HomePage from "../screens/HomePage/Home"
 import Biomas from "../screens/BiomasPage/Biomas";
 import Journal from "../screens/JournalPage/Journal";
-import ProfilePage from "../screens/ProfilePage";
-import RegisterPage from "../screens/RegisterPage";
-import LoginPage from "../screens/LoginPage";
-import Lessons from "../screens/Lessons"
-import Game from "../screens/Game"
+import ProfilePage from "../screens/ProfilePage/ProfilePage";
+import RegisterPage from "../screens/RegisterPage/RegisterPage";
+import LoginPage from "../screens/LoginPage/LoginPage";
+import Lessons from "../screens/LessonsPage/Lessons"
+import Game from "../screens/GamePage/Game"
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />}></Route>
+        <Route path="/Home" element={<HomePage />}></Route>
         <Route path="/Biomas" element={<Biomas />}></Route>
         <Route path="/Journal" element={<Journal />}></Route>
         <Route path="/Profile" element={<ProfilePage />}></Route>
--- src/components-screens/Banner/Banner.jsx
+++ src/components-screens/Banner/Banner.jsx
@@ -1,3 +1,4 @@
+import { Box } from '@mui/material';
 import './Banner.css';
 import { Link } from "react-router-dom";
 const Banner = () => {
