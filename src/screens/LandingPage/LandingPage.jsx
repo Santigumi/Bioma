99613c524583753a67c9076c2e6bdd040180f0ea
Santigumi <santymillan95@gmail.com>
2025-05-07 16:20:59 -0500

    fix landingPage and add breakpoints

--- src/screens/LandingPage/LandingPage.jsx
+++ src/screens/LandingPage/LandingPage.jsx
@@ -1,18 +1,30 @@
-import Navbar from '../../components-screens/Navbar/Navbar';
-import Banner from '../../components-screens/Banner/Banner';
-import backgroundImage from '../../assets/backgrounds/fondolanding.jpg';
-import './LandingPage.css';
-
+import Navbar from "../../components-screens/Navbar/Navbar";
+import Banner from "../../components-screens/Banner/Banner";
+import backgroundImage from "../../assets/backgrounds/fondolanding.jpg";
+import { Box } from "@mui/material";
 const LandingPage = () => {
-    return(
-        <>
-            <div className='background' style={{ backgroundImage: `url(${backgroundImage})` }}>
-                <Navbar></Navbar>
-                <Banner></Banner>
-            </div>
-
-        </>
-    )
-}
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: {
+          xs: "column-reverse",
+          sm: "column-reverse",
+          md: "column-reverse",
+          lg: "row",
+          xl: "row",
+        },
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        height: "100vh",
+        width: "100vw",
+        backgroundImage: `url(${backgroundImage})`,
+      }}
+    >
+      <Navbar></Navbar>
+      <Banner></Banner>
+    </Box>
+  );
+};
 
 export default LandingPage;
