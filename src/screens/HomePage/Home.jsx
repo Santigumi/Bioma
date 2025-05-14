211481731e578e1638ef442dc22647f9775b19c1
Santigumi <santymillan95@gmail.com>
2025-05-13 20:26:41 -0500

    Update Home

--- src/screens/HomePage/Home.jsx
+++ src/screens/HomePage/Home.jsx
@@ -1,20 +1,101 @@
-import Navbar from '../../components-screens/Navbar/Navbar';
-import HomeBanner from '../../components-screens/HomeBanner/HomeBanner';
-import { Box } from "@mui/material";
+import Navbar from "../../components-screens/Navbar/Navbar";
+import { ThemeProvider } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { Box, Typography, Grid, Button } from "@mui/material";
+import theme from "../../Themes/Theme";
 const Home = () => {
-    return(
-        <Box sx={{
-            display: 'flex',
-            flexDirection: {
-            xs:"column",
-            md:"column",
-            lg:"row",
-            xl:"row",
-            }
-        }}>
+  const navigate = useNavigate();
+  return (
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: {
+            xs: "column",
+            md: "column",
+            lg: "row",
+            xl: "row",
+            backgroundImage: ``
+          },
+        }}
+      >
         <Navbar></Navbar>
-        <HomeBanner></HomeBanner>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            height: "100vh",
+            width: "80vw",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              alignItems: "center",
+              height: "90%",
+              width: "100%",
+              backgroundColor: "#45C484",
+              borderRadius: "2rem",
+              paddingTop: "1rem",
+              paddingBottom: "1rem",
+            }}
+          >
+            <Box
+              sx={{
+                height: "21rem",
+                width: "73rem",
+                backgroundColor: "yellow",
+                borderRadius: "2rem",
+              }}
+            >
+              <Typography>Welcome back, your name</Typography>
+            </Box>
+            <Grid container spacing={3}>
+              <Button
+                onClick={() => navigate("/Lessons")}
+                sx={{
+                  width: "23.5rem",
+                  height: "15rem",
+                  backgroundColor: "black",
+                  borderRadius: "2rem",
+                  color: "white",
+                }}
+              >
+                Continue your lessons
+              </Button>
+              <Button
+                onClick={() => navigate("/Lessons")}
+                sx={{
+                  width: "23.5rem",
+                  height: "15rem",
+                  backgroundColor: "black",
+                  borderRadius: "2rem",
+                  color: "white",
+                }}
+              >
+                Trophy store
+              </Button>
+
+              <Button
+                onClick={() => navigate("/Lessons")}
+                sx={{
+                  width: "23.5rem",
+                  height: "15rem",
+                  backgroundColor: "black",
+                  borderRadius: "2rem",
+                  color: "white",
+                }}
+              >
+                You can wacth, read
+              </Button>
+            </Grid>
+          </Box>
         </Box>
-    )
-}
-export default Home
+      </Box>
+    </ThemeProvider>
+  );
+};
+export default Home;
