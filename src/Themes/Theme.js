ceb3a01e78bc33773aa9b29a438e5187476cac70
Santigumi <santymillan95@gmail.com>
2025-05-13 22:40:09 -0500

    added ui kit colors

--- src/Themes/Theme.js
+++ src/Themes/Theme.js
@@ -3,6 +3,24 @@ const theme = createTheme({
   typography: {
     fontFamily: `'Livvic', sans-serif`, // Usa el nombre de la fuente
   },
+  palette: {
+    green: {
+      main: "#00E773",
+      contrastText: "#ffffff",
+    },
+    yellow: {
+      main: "#FFEE8B",
+      contrastText: "#fff",
+    },
+    blue: {
+      main: "#4AB8F0",
+      contrastText: "#fff",
+    },
+    red: {
+      main: "#D62828",
+      contrastText: "#fff",
+    },
+  },
 });
 
 export default theme;
