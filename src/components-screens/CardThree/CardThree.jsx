3032cb636a3886e09e54d2491dc2d9c50fce20e1
Santigumi <santymillan95@gmail.com>
2025-05-13 21:08:00 -0500

    Update CardThree

--- src/components-screens/CardThree/CardThree.jsx
+++ src/components-screens/CardThree/CardThree.jsx
@@ -5,7 +5,7 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
- const CardThree = (price) => {
+ const CardThree = ({price}) => {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
