811aa7133859fef370401456f0e2598e6556eefd
Santigumi <santymillan95@gmail.com>
2025-05-13 21:00:47 -0500

    Create CardThree

---
+++ src/components-screens/CardThree/CardThree.jsx
@@ -0,0 +1,32 @@
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+ const CardThree = (price) => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image=""
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5">
+          Capybara
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Buy: ${price}</Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default CardThree 
