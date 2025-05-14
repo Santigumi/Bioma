65ca07156d73a75f293a64b6897645add49f2f79
Santigumi <santymillan95@gmail.com>
2025-05-13 22:51:22 -0500

    update styles for CardThree

--- src/components-screens/CardThree/CardThree.jsx
+++ src/components-screens/CardThree/CardThree.jsx
@@ -9,27 +9,22 @@ import { ThemeProvider } from "@mui/material";
 const CardThree = ({ price }) => {
   return (
     <ThemeProvider theme={theme}>
-      <Card sx={{ maxWidth: 345 }}>
+      <Card sx={{ maxWidth: 345, borderRadius: 7.5 }}>
         <CardMedia sx={{ height: 140 }} image="" title="green iguana" />
-        <CardContent>
+        <CardContent sx={{display: 'flex', flexDirection:'row', justifyContent:'space-between'}}>
           <Typography gutterBottom variant="h5">
             Capybara
           </Typography>
-        </CardContent>
-        <CardActions sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'end'
-        }}>
           <Button
             size="medium"
-            sx={{ backgroundColor: theme.palette.green.main,
-                color: theme.palette.green.contrastText
-             }}
+            sx={{
+              backgroundColor: theme.palette.green.main,
+              color: theme.palette.green.contrastText,
+            }}
           >
             Buy: ${price}
           </Button>
-        </CardActions>
+        </CardContent>
       </Card>
     </ThemeProvider>
   );
