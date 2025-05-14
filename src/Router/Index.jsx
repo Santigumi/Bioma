89a73fd731485eb7c341d3deb940d813d908d888
Mariana <marianitalizcanor@gmail.com>
2025-05-14 15:43:10 -0500

    feat: Implement authentication flow with ProtectedRoute and integrate Redux store

--- src/Router/Index.jsx
+++ src/Router/Index.jsx
@@ -9,20 +9,94 @@ import LoginPage from "../screens/LoginPage/LoginPage";
 import Lessons from "../screens/LessonsPage/Lessons";
 import Game from "../screens/GamePage/Game";
 import Trophy from "../screens/TrophyPage/Trophy";
+
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { auth } from "../services/firebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
+import { clearUser, setUser } from "../redux/auth/AuthSlice";
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
+
 const Router = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(setUser(user.uid));
+      } else {
+        dispatch(clearUser());
+      }
+    });
+
+    //Desmontar al terminarse de ejecutar
+    return () => unsubscribe();
+  }, [dispatch]);
+
   return (
     <BrowserRouter>
       <Routes>
+        {/*Rutas normales*/}
         <Route path="/" element={<LandingPage />}></Route>
-        <Route path="/Home" element={<HomePage />}></Route>
-        <Route path="/Biomas" element={<Biomas />}></Route>
-        <Route path="/Journal" element={<Journal />}></Route>
-        <Route path="/Profile" element={<ProfilePage />}></Route>
         <Route path="/Login" element={<LoginPage />}></Route>
         <Route path="/Register" element={<RegisterPage />}></Route>
-        <Route path="/Lessons" element={<Lessons />}></Route>
-        <Route path="/Game" element={<Game />}></Route>
-        <Route path="/Trophy" element={<Trophy />}></Route>
+        {/*Rutas protegidas*/}
+        <Route
+          path="/Home"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        ></Route>
+        <Route
+          path="/Biomas"
+          element={
+            <ProtectedRoute>
+              <Biomas />
+            </ProtectedRoute>
+          }
+        ></Route>
+        <Route
+          path="/Journal"
+          element={
+            <ProtectedRoute>
+              <Journal />
+            </ProtectedRoute>
+          }
+        ></Route>
+        <Route
+          path="/Profile"
+          element={
+            <ProtectedRoute>
+              <ProfilePage />
+            </ProtectedRoute>
+          }
+        ></Route>
+        <Route
+          path="/Lessons"
+          element={
+            <ProtectedRoute>
+              <Lessons />
+            </ProtectedRoute>
+          }
+        ></Route>
+        <Route
+          path="/Game"
+          element={
+            <ProtectedRoute>
+              <Game />
+            </ProtectedRoute>
+          }
+        ></Route>
+        <Route
+          path="/Trophy"
+          element={
+            <ProtectedRoute>
+              <Trophy />
+            </ProtectedRoute>
+          }
+        ></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import CounterReducer from "./counter/CounterSlice.js";
 import AuthReducer from "./auth/AuthSlice.js";
 
 export const store = configureStore({
   reducer: {
-    counter: CounterReducer,
     auth: AuthReducer,
   },
 });
