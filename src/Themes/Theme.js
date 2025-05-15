44c9f913889714463240f2eb7f4d8997c30b0d24
Santigumi <santymillan95@gmail.com>
2025-05-15 13:56:59 -0500

    Update theme

--- src/Themes/Theme.js
+++ src/Themes/Theme.js
@@ -1,7 +1,7 @@
 import { createTheme } from "@mui/material";
 const theme = createTheme({
   typography: {
-    fontFamily: `'Livvic', sans-serif`, // Usa el nombre de la fuente
+    fontFamily: `'Livvic', sans-serif`
   },
   palette: {
     green: {
