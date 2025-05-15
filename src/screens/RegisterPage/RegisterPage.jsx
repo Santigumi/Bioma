8cb9497c611d298c668b5e89d73c89dfe2338285
Santigumi <santymillan95@gmail.com>
2025-05-14 23:50:37 -0500

    Update sizes

--- src/screens/RegisterPage/RegisterPage.jsx
+++ src/screens/RegisterPage/RegisterPage.jsx
@@ -1,35 +1,34 @@
 import Navbar from "../../components-screens/Navbar/Navbar";
 import RegisterForm from "../../components-screens/RegisterForm/RegisterForm";
 import backgroundImage from "../../assets/backgrounds/fondoregister.webp";
-import {
-  Box,
-  ThemeProvider
-} from "@mui/material";
+import { Box, ThemeProvider } from "@mui/material";
 const RegisterPage = () => {
   return (
+    <Box
+      className="background"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <Navbar></Navbar>
       <Box
-        className="background"
-        style={{ backgroundImage: `url(${backgroundImage})` }}
-      >
-        <Navbar></Navbar>
-        <Box sx={{
-          display:'flex',
-          flexDirection:'column',
+        sx={{
+          display: "flex",
+          flexDirection: "column",
           justifyContent: "center",
-          alignItems: 'center',
+          alignItems: "center",
           width: {
-          xs:'100%',
-          sm:'100%',
-          md:'80vw',
-          lg:'80vw',
-          xl:'80vw'
+            xs: "100%",
+            sm: "100%",
+            md: "80vw",
+            lg: "80vw",
+            xl: "80vw",
           },
-          height:'100vh',
-          border: 1
-        }}>
-          <RegisterForm></RegisterForm>
-        </Box>
+          height: "100vh",
+          border: 1,
+        }}
+      >
+        <RegisterForm></RegisterForm>
       </Box>
+    </Box>
   );
 };
 
