3c2b1592879a533bfc71d44a43d427b93f37e295
Santigumi <santymillan95@gmail.com>
2025-05-15 11:47:38 -0500

    Update loginPage

--- src/screens/LoginPage/LoginPage.jsx
+++ src/screens/LoginPage/LoginPage.jsx
@@ -1,33 +1,36 @@
 import Navbar from "../../components-screens/Navbar/Navbar";
 import LoginForm from "../../components-screens/LoginForm/LoginForm";
 import backgroundImage from "../../assets/backgrounds/fondoregister.webp";
-import { Box } from "@mui/material";
+import { Box, ThemeProvider } from "@mui/material";
+import theme from "../../Themes/Theme";
 const LoginPage = () => {
   return (
-    <Box
-      className="background"
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-    >
-      <Navbar></Navbar>
+    <ThemeProvider theme={theme}>
       <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          width: {
-            xs: "100%",
-            sm: "100%",
-            md: "80vw",
-            lg: "80vw",
-            xl: "80vw",
-          },
-          height: '100vh',
-        }}
+        className="background"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
       >
-        <LoginForm />
+        <Navbar backgroundColor={theme.palette.red.secondary}></Navbar>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            width: {
+              xs: "100%",
+              sm: "100%",
+              md: "80vw",
+              lg: "80vw",
+              xl: "80vw",
+            },
+            height: "100vh",
+          }}
+        >
+          <LoginForm />
+        </Box>
       </Box>
-    </Box>
+    </ThemeProvider>
   );
 };
 
