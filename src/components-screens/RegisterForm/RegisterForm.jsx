a8d07d79c550177b953baacbbaccff6fe3c10012
Santigumi <santymillan95@gmail.com>
2025-05-14 21:58:59 -0500

    add mui v5

--- src/components-screens/RegisterForm/RegisterForm.jsx
+++ src/components-screens/RegisterForm/RegisterForm.jsx
@@ -3,6 +3,16 @@ import "./RegisterForm.css";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { registerUser } from "../../services/firebaseUtils";
+import {
+  Box,
+  Typography,
+  ThemeProvider,
+  TextField,
+  Button,
+} from "@mui/material";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 const RegisterForm = () => {
   const navigate = useNavigate();
@@ -49,87 +59,162 @@ const RegisterForm = () => {
   };
 
   return (
-    <div className="register-container">
-      <h2>Create your account</h2>
-      {registerError && <div className="error-alert">{registerError}</div>}
-      <form className="register-form" onSubmit={handleSubmit(onSubmit)}>
-        <div className="form-group">
-          <label>Username</label>
-          <input
-            type="text"
-            {...register("username", { required: "Username is required" })}
-          />
-          {errors.username && (
-            <p className="error-message">{errors.username.message}</p>
-          )}
-        </div>
-        <div className="form-group">
-          <label>Create password</label>
-          <input
-            type="password"
-            {...register("password", {
-              required: "Password is required",
-              minLength: {
-                value: 8,
-                message: "Password must be at least 8 characters",
-              },
-            })}
-          />
-          {errors.password && (
-            <p className="error-message">{errors.password.message}</p>
-          )}
-        </div>
-        <div className="form-group">
-          <label>Birthday</label>
-          <input
-            type="date"
-            {...register("birthday", { required: "Birthday is required" })}
-          />
-          {errors.birthday && (
-            <p className="error-message">{errors.birthday.message}</p>
-          )}
-        </div>
-        <div className="form-group">
-          <label>Confirm password</label>
-          <input
-            type="password"
-            {...register("confirmPassword", {
-              required: "Please confirm your password",
-              validate: (value) =>
-                value === password || "Passwords do not match",
-            })}
-          />
-          {errors.confirmPassword && (
-            <p className="error-message">{errors.confirmPassword.message}</p>
-          )}
-        </div>
-        <div className="form-group">
-          <label>Email</label>
-          <input
-            type="email"
-            {...register("email", {
-              required: "Email is required",
-              pattern: {
-                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                message: "Please enter a valid email",
-              },
-            })}
-          />
-          {errors.email && (
-            <p className="error-message">{errors.email.message}</p>
-          )}
-        </div>
-        <button type="submit" className="register-button" disabled={isLoading}>
-          {isLoading ? "Registrando..." : "Registrarse"}
-        </button>
-        <div className="form-submit">
-          <Link to="/Login">Already have an account? </Link>
-          <Link id="Signin" to="/Login">
-            Log in
-          </Link>
-        </div>
-      </form>
-    </div>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          backgroundColor: "rgb(244, 216, 216)",
+          borderRadius: "20px",
+          width: "70vw",
+          height: "80vh",
+          boxShadow: "0.1rem 0.1rem 1rem 0.1rem rgb(133, 132, 132)",
+          border: 5,
+          borderColor: "rgb(251, 86, 86)",
+        }}
+      >
+        <Typography
+          variant="h4"
+          sx={{ display: 'flex', flexDirection:'column', height: "30%", justifyContent: "center", paddingLeft: '3rem' }}
+        >
+          Create your account
+        </Typography>
+
+        {registerError && <div className="error-alert">{registerError}</div>}
+        <form
+          style={{ height: "90%" }}
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              height: "90%",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                width: "50%",
+                justifyContent: "start",
+                alignItems: "center",
+                gap: 3,
+              }}
+            >
+              <Box sx={{ width: "80%" }}>
+                <TextField
+                  sx={{ width: "100%" }}
+                  label="Username"
+                  type="text"
+                  {...register("username", {
+                    required: "Username is required",
+                  })}
+                />
+                {errors.username && (
+                  <p className="error-message">{errors.username.message}</p>
+                )}
+              </Box>
+              <Box sx={{ width: "80%" }}>
+                <TextField
+                  sx={{ width: "100%" }}
+                  label="Email"
+                  type="email"
+                  {...register("email", {
+                    required: "Email is required",
+                    pattern: {
+                      value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                      message: "Please enter a valid email",
+                    },
+                  })}
+                />
+                {errors.email && (
+                  <p className="error-message">{errors.email.message}</p>
+                )}
+              </Box>
+              <Box sx={{ width: "80%" }}>
+                <DatePicker
+                  sx={{ width: "100%" }}
+                  label="Birthday"
+                  {...register("birthday", {
+                    required: "Birthday is required",
+                  })}
+                />
+                {errors.birthday && (
+                  <p className="error-message">{errors.birthday.message}</p>
+                )}
+              </Box>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                width: "50%",
+                justifyContent: "start",
+                alignItems: "center",
+                gap: 3,
+              }}
+            >
+              <Box sx={{ width: "80%" }}>
+                <TextField
+                  sx={{ width: "100%" }}
+                  label="Create password"
+                  type="password"
+                  {...register("password", {
+                    required: "Password is required",
+                    minLength: {
+                      value: 8,
+                      message: "Password must be at least 8 characters",
+                    },
+                  })}
+                />
+                {errors.password && (
+                  <p className="error-message">{errors.password.message}</p>
+                )}
+              </Box>
+
+              <Box sx={{ width: "80%" }}>
+                <TextField
+                  sx={{ width: "100%" }}
+                  label="Confirm password"
+                  {...register("confirmPassword", {
+                    required: "Please confirm your password",
+                    validate: (value) =>
+                      value === password || "Passwords do not match",
+                  })}
+                />
+                {errors.confirmPassword && (
+                  <p className="error-message">
+                    {errors.confirmPassword.message}
+                  </p>
+                )}
+              </Box>
+
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  height: "5%",
+                  width: "80%",
+                }}
+              >
+                <Link to="/Login">Already have an account? </Link>
+              
+              </Box>
+              <Button
+                sx={{ height: "15%", width: "80%", border: 1, }}
+                type="submit"
+                className="register-button"
+                disabled={isLoading}
+              >
+                {isLoading ? "Registrando..." : "Registrarse"}
+              </Button>
+
+            </Box>
+          </Box>
+        </form>
+      </Box>
+    </LocalizationProvider>
   );
 };
 
