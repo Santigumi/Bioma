d74b9409e0e111ed45b0c3ef6ac63104b14fb2ec
Mariana <marianitalizcanor@gmail.com>
2025-05-22 18:01:15 -0500

    feat: Update Firebase configuration to use environment variables and modify allowedHosts in Vite config

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -3,17 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBMUL1Kmauj-fsnX7DRvsR3n8GySzefkhg",
-  authDomain: "bioma-5ccd5.firebaseapp.com",
-  projectId: "bioma-5ccd5",
-  storageBucket: "bioma-5ccd5.appspot.com",
-  messagingSenderId: "190666568247",
-  appId: "1:190666568247:web:f7ba83c401677f6176c5b2",
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 export const appFirebase = initializeApp(firebaseConfig);
 export const auth = getAuth(appFirebase);
 export const db = getFirestore(appFirebase);
