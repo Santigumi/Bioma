186b3f8cfeeceb2be3f45c0759791d1eaf9a5224
Santigumi <santymillan95@gmail.com>
2025-05-24 18:53:18 -0500

    Create dinamic sizes

--- src/components-Game/Level/Level.jsx
+++ src/components-Game/Level/Level.jsx
@@ -1,16 +1,51 @@
 import { Stage } from "react-konva";
 import Map from "../Map/Map";
 import Player from "../Player/Player";
-import { GRID_MAP, TITLE_SIZE } from "../../utils/constants";
+import { GRID_MAP } from "../../utils/constants";
+import { useRef, useState, useEffect } from "react";
 
-const Level = () => {
-  const mapWidth = GRID_MAP[0].length;
-  const mapHeight = GRID_MAP.length
+const Level = ({ onVictory }) => {
+
+  const containerRef = useRef();
+  const [dimensions, setDimensions] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const updateSize = () => {
+      if (containerRef.current) {
+        setDimensions({
+          width: containerRef.current.offsetWidth,
+          height: containerRef.current.offsetHeight,
+        });
+        setLoading(false);
+      }
+    };
+    setTimeout(() => {
+      updateSize();
+      window.addEventListener("resize", updateSize);
+    }, 300);
+
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+
+  if (loading || !dimensions) {
+    return <div ref={containerRef}>Loading map</div>;
+  }
+  const mapWidth = GRID_MAP[0].length || 1;
+  const mapHeight = GRID_MAP.length || 1;
+  const tileSize = Math.floor(
+    Math.min(dimensions.width / mapWidth, dimensions.height / mapHeight)
+  );
+
   return (
-    <Stage width={mapWidth * TITLE_SIZE} height={mapHeight * TITLE_SIZE}>
-      <Map grid={GRID_MAP} />
-      <Player grid={GRID_MAP} />
-    </Stage>
+    <div
+      ref={containerRef}
+      style={{ width: "100vw", height: "100vh", overflow: "hidden" }}
+    >
+      <Stage width={dimensions.width} height={dimensions.height}>
+        <Map grid={GRID_MAP} tileSize={tileSize} />
+        <Player grid={GRID_MAP} onVictory={onVictory} tileSize={tileSize} />
+      </Stage>
+    </div>
   );
 };
 
