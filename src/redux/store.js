325a2124804d49ec37cd1fb1e3b1b0aa77819d6d
Santigumi <santymillan95@gmail.com>
2025-06-04 10:38:11 -0500

    fixed route of itemsSlice.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AuthReducer from "./auth/AuthSlice.js";
-import itemsReducer from './itemsSlice';
+import itemsReducer from './game/itemsSlice.js';
 export const store = configureStore({
   reducer: {
     auth: AuthReducer,
