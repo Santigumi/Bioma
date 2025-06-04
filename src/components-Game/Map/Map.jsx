87c0beaf5dd6f377107f6071712aa9a94e85c52a
Santigumi <santymillan95@gmail.com>
2025-06-04 10:50:28 -0500

    added collect items logic

--- src/components-Game/Map/Map.jsx
+++ src/components-Game/Map/Map.jsx
@@ -1,44 +1,45 @@
 import { Layer, Image as KonvaImage } from "react-konva";
 import useImage from "use-image";
-
+import { useSelector } from "react-redux";
 const Map = ({ grid, tileSize, offsetX = 0, offsetY = 0 }) => {
   const [pastoImage] = useImage("../src/assets/Sprites/Pasto.png");
   const [arbolImage] = useImage("../src/assets/Sprites/PlantaSavanah.png");
+  const [itemImage] = useImage("../src/assets/Sprites/moneda.webp")
   const roundedTileSize = Math.round(tileSize);
-  console.log(roundedTileSize);
-  
-  return (
+  const items = useSelector((state) => state.items.items);
+ return (
     <Layer x={Math.round(offsetX)} y={Math.round(offsetY)}>
       {grid.map((row, y) =>
         row.map((cell, x) => {
           const posX = x * tileSize;
           const posY = y * tileSize;
 
-          if (cell === 1) {
-            return (
-              <KonvaImage
-                key={`${x}-${y}`}
-                x={posX - 0.5}
-                y={posY - 0.5}
-                width={tileSize + 1}
-                height={tileSize + 1 }
-                image={arbolImage}
-              ></KonvaImage>
-            );
-          }
+          const isFloor = cell === 0 || cell === 2 || cell === 3;
 
           return (
             <KonvaImage
-              key={`${x}-${y}`}
+              key={`tile-${x}-${y}`}
               x={posX - 0.5}
               y={posY - 0.5}
               width={roundedTileSize + 1}
               height={roundedTileSize + 1}
-              image={pastoImage}
-            ></KonvaImage>
+              image={isFloor ? pastoImage : arbolImage}
+            />
           );
         })
       )}
+
+      {/* Dibuja los ítems sobre el mapa */}
+      {items.map(({ x, y }) => (
+        <KonvaImage
+          key={`item-${x}-${y}`}
+          x={x * tileSize}
+          y={y * tileSize}
+          width={tileSize}
+          height={tileSize}
+          image={itemImage}
+        />
+      ))}
     </Layer>
   );
 };
