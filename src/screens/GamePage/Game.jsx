27ba29e6485d7d872b2b8225c7704099bd245e60
Santigumi <santymillan95@gmail.com>
2025-06-06 21:25:10 -0500

    Update Game

--- src/screens/GamePage/Game.jsx
+++ src/screens/GamePage/Game.jsx
@@ -1,22 +1,26 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import Level from "../../components-Game/Level/Level";
 import Modal from "@mui/material/Modal";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-const Game = () => {
+const Game = ({ background, characters, map, text, color }) => {
   const navigate = useNavigate();
   const [openStart, setOpenStart] = useState(true);
   const [openPause, setOpenPause] = useState(false);
   const [openVictory, setVictoryModal] = useState(false);
   const items = useSelector((state) => state.items.items);
-  useEffect(()=>{
-    if(items.length === 0){
-      setVictoryModal(true)
+  useEffect(() => {
+    if (items.length === 0) {
+      setVictoryModal(true);
     }
-  },[items]);
+  }, [items]);
   const modalStyle = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    gap: 3,
     position: "absolute",
     top: "50%",
     left: "50%",
@@ -24,7 +28,22 @@ const Game = () => {
     bgcolor: "white",
     padding: 4,
     borderRadius: 2,
-    textAlign: "center",
+  };
+  const startModalStyle = {
+    display: "flex",
+    flexDirection: "row",
+    position: "absolute",
+    bottom: 20,
+    left: "50%",
+    transform: "translateX(-50%)",
+    bgcolor: "white",
+    padding: 3,
+    borderRadius: "1rem",
+    width: "80vw",
+    height: "15vh",
+    textAlign: "start",
+    alignItems: "center",
+    boxShadow: 24,
   };
   return (
     <Box
@@ -38,34 +57,63 @@ const Game = () => {
         overflow: "hidden",
         position: "relative",
         backgroundImage:
-          "url('../src/assets/backgrounds/Biomas-Background.png')",
+           "url('../src/assets/backgrounds/Biomas-Background.webp')",
       }}
     >
       <Button
-        sx={{ position: "absolute", top: 10, right: 10, zIndex: 10 }}
+        sx={{
+          position: "absolute",
+          top: 10,
+          right: 10,
+          zIndex: 10,
+          backgroundColor: "#FFE549",
+          color: "#000000",
+          boxShadow: 3,
+        }}
         onClick={() => setOpenPause(true)}
       >
         ||
       </Button>
       <Modal open={openStart} onClose={() => setOpenStart(false)}>
-        <Box sx={modalStyle}>
-          <h2>¡Bienvenido!</h2>
-          <Button onClick={() => setOpenStart(false)}>Iniciar</Button>
+        <Box sx={startModalStyle}>
+          <Typography variant="h3">¡Bienvenido!</Typography>
+          <Button
+            sx={{ backgroundColor: "#FFE549", color: "#000000" }}
+            onClick={() => setOpenStart(false)}
+          >
+            Entendido
+          </Button>
         </Box>
       </Modal>
 
       <Modal open={openPause} onClose={() => setOpenPause(false)}>
         <Box sx={modalStyle}>
-          <h2>Juego en Pausa</h2>
-          <Button onClick={() => setOpenPause(false)}>Continuar</Button>
-          <Button onClick={() => window.location.reload()}>Reiniciar</Button>
+          <Typography variant="h4" sx={{ color: "#000000" }}>
+            Juego en Pausa
+          </Typography>
+          <Box sx={{display:"flex", flexDirection:"row", justifyContent:"space-around"}}>
+            <Button
+              sx={{ backgroundColor: "#FFE549", color: "#000000" }}
+              onClick={() => setOpenPause(false)}
+            >
+              Continuar
+            </Button>
+            <Button
+              sx={{ backgroundColor: "#FFE549", color: "#000000" }}
+              onClick={() => navigate("/Lessons")}
+            >
+              Salir
+            </Button>
+          </Box>
         </Box>
       </Modal>
 
       <Modal open={openVictory} onClose={() => {}}>
         <Box sx={modalStyle}>
           <h2>¡Ganaste!</h2>
-          <Button onClick={() => navigate("/Lessons")}>Volver a jugar</Button>
+          <Button 
+          sx={{ backgroundColor: "#FFE549", color: "#000000" }}
+          onClick={() => navigate("/Lessons")}>Continuar</Button>
         </Box>
       </Modal>
       <Box
