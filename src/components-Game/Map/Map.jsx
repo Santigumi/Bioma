d5c0b3a60de8773369e932f5504bf277104987ff
Santigumi <santymillan95@gmail.com>
2025-06-08 23:08:29 -0500

    Update map for create leves

--- src/components-Game/Map/Map.jsx
+++ src/components-Game/Map/Map.jsx
@@ -1,20 +1,31 @@
 import { Layer, Image as KonvaImage } from "react-konva";
 import useImage from "use-image";
 import { useSelector } from "react-redux";
-const Map = ({ grid, tileSize, offsetX = 0, offsetY = 0 }) => {
-  const [pastoImage] = useImage("../src/assets/Sprites/Pasto.png");
-  const [arbolImage] = useImage("../src/assets/Sprites/PlantaSavanah.png");
-  const [itemImage] = useImage("../src/assets/Sprites/moneda.webp")
+const Map = ({ tileSize, offsetX = 0, offsetY = 0, sprites, mapData }) => {
+
+  const [Obstaculo1Image] = useImage(sprites.Obstacule1);
+  const [Obstaculo2Image] = useImage(sprites.Obstacule2);
+  const [FloorImage] = useImage(sprites.Floor);
+  const [itemImage] = useImage("../src/assets/Sprites/moneda.webp");
   const roundedTileSize = Math.round(tileSize);
   const items = useSelector((state) => state.items.items);
- return (
+  console.log(FloorImage)
+  return (
     <Layer x={Math.round(offsetX)} y={Math.round(offsetY)}>
-      {grid.map((row, y) =>
+      {mapData.map((row, y) =>
         row.map((cell, x) => {
           const posX = x * tileSize;
           const posY = y * tileSize;
 
-          const isFloor = cell === 0 || cell === 2 || cell === 3;
+          let imageToUse;
+
+          if (cell === 0 || cell === 2 || cell === 3) {
+            imageToUse = FloorImage;
+          } else if (cell === 4) {
+            imageToUse = Obstaculo1Image;
+          } else {
+            imageToUse = Obstaculo2Image;
+          }
 
           return (
             <KonvaImage
@@ -23,13 +34,11 @@ const Map = ({ grid, tileSize, offsetX = 0, offsetY = 0 }) => {
               y={posY - 0.5}
               width={roundedTileSize + 1}
               height={roundedTileSize + 1}
-              image={isFloor ? pastoImage : arbolImage}
+              image={imageToUse}
             />
           );
         })
       )}
-
-      {/* Dibuja los ítems sobre el mapa */}
       {items.map(({ x, y }) => (
         <KonvaImage
           key={`item-${x}-${y}`}
