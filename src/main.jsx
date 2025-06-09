9b090c9233a595edce8e6dd208cf5b1f39100cef
Santigumi <santymillan95@gmail.com>
2025-06-09 14:01:44 -0500

    fix error

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,6 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
 import { store } from "./redux/store.js";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
