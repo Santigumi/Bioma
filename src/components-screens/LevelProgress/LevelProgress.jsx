33388288690d4d329ae89a494a6733c97068fd99
Santigumi <santymillan95@gmail.com>
2025-06-09 14:14:56 -0500

    update styles

--- src/components-screens/LevelProgress/LevelProgress.jsx
+++ src/components-screens/LevelProgress/LevelProgress.jsx
@@ -2,15 +2,21 @@ import "./LevelProgress.css";
 import { Box, Typography } from "@mui/material";
 const LevelProgress = ({ iconProgress, percentage, color }) => {
   return (
-    <Box sx={{ backgroundColor: color || `rgba(10, 191, 100, 1)`, 
-    width: "100%",
-    height: "80%"
-    }}>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: color || `rgba(10, 191, 100, 1)`,
+        width: "100%",
+        height: "80%",
+        borderRadius: 10,
+      }}
+    >
       <Typography>{percentage}</Typography>
     </Box>
   );
 };
 
 export default LevelProgress;
-
-
